6cbe077963e89ca6f40c65c90bb785466efa7982
Grzegorz <grzegorz.popiel@yahoo.com>
2020-06-08 18:18:01 +0200

    Fix serverUrl.

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,4 +1,4 @@
-export const serverUrl = `http://localhost:8080`
+export const serverUrl = 'https://sms-rest-app.herokuapp.com'
 
 export const auth = `${serverUrl}/api/auth`;
 export const signUp = `${serverUrl}/api/v1/user/signup`;
